8d31c878120f6e022919fe5eb9e3e3da75a76467
SergeyH <hatsanyuks@gmail.com>
2015-02-13 10:16:42 +0200

    added Magentostudy module

---
+++ app/code/community/Magentostudy/News/Helper/Admin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * News Admin helper
+ */
+class Magentostudy_News_Helper_Admin extends Mage_Core_Helper_Abstract
+{
+    /**
+     * Check permission for passed action
+     *
+     * @param string $action
+     * @return bool
+     */
+    public function isActionAllowed($action)
+    {
+        return Mage::getSingleton('admin/session')->isAllowed('news/manage/' . $action);
+    }
+}
---
+++ app/code/community/Magentostudy/News/Model/News.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * News item model
+ */
+class Magentostudy_News_Model_News extends Mage_Core_Model_Abstract
+{
+    /**
+     * Define resource model
+     */
+    protected function _construct()
+    {
+        $this->_init('magentostudy_news/news');
+    }
+
+    /**
+     * If object is new adds creation date
+     *
+     * @return Magentostudy_News_Model_News
+     */
+    protected function _beforeSave()
+    {
+        parent::_beforeSave();
+        if ($this->isObjectNew()) {
+            $this->setData('created_at', Varien_Date::now());
+        }
+        return $this;
+    }
+}
